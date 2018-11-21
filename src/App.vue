54a6b52429d2a9a1bc1fc9be48317afdcc21a30b
LeoooY <Leoyy3@163.com>
2018-11-22 01:08:53 +0800

    uodate

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,47 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <myNav></myNav>
+
+    <div id="main">
+      <div>
+        <img src="./assets/logo.png">
+        <p>
+          If Element is successfully added to this project, you'll see an
+          <code v-text="'<el-button>'"></code>
+          below
+        </p>
+        <el-button>el-button</el-button>
+      </div>
+
+      <HelloWorld msg="Welcome to Your Vue.js App" />
     </div>
-    <router-view/>
   </div>
 </template>
 
+<script>
+  import HelloWorld from './components/HelloWorld.vue'
+  import myNav from './components/myNav.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      HelloWorld,
+      myNav
+    }
+  }
+</script>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  #main {
+    position: relative;
+    top: 60px;
+  }
 </style>
